48b35c47a2fb7d935e5833c3aed4c4194ebaf0ea
Dead End <dead.end@inbox.ru>
2025-03-16 08:53:27 +0100

    css

--- src/routes/Checkbox.svelte
+++ src/routes/Checkbox.svelte
@@ -8,7 +8,7 @@
 		{name}
 		type="radio"
 		{checked}
-		class="_transition _outline relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-blue-600 checked:bg-blue-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden"
+		class="_transition _outline _border relative size-4 appearance-none bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-blue-600 checked:bg-blue-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden"
 	/>
 	<label for={id} class="block text-sm/6 font-medium text-gray-900">{label}</label>
 </div>
@@ -22,5 +22,9 @@
 
 	._outline {
 		@apply focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-blue-600;
+	}
+
+	._border {
+		@apply rounded-full border border-gray-300;
 	}
 </style>
