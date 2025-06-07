ce14d99c5ab1549488c1087398a45beabc9082bb
Dead End <dead.end@inbox.ru>
2025-06-07 09:06:56 +0200

    Removed Children

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -6,33 +6,28 @@
 	let surname = $state('');
 	let email = $state('');
 	let person = $state(0);
-	let children = $state(0);
 
 	let part = $state('party');
 	let error = $state('');
 
-	const doText = (personen: number, kinder: number) => {
-		if (personen === 1 && kinder === 0) {
+	const doText = (personen: number) => {
+		if (personen === 1) {
 			return 'Ich komme alleine';
 		}
-		return `Wir kommen mit ${personen} Erwachsenen und ${kinder} Kindern`;
+		return `Wir kommen mit ${personen} Personen`;
 	};
 
-	const doSubject = (personen: number, kinder: number, name: string) => {
-		return `${doText(personen, kinder)} - ${name}`;
+	const doSubject = (personen: number, name: string) => {
+		return `${doText(personen)} - ${name}`;
 	};
 
 	// @ts-ignore
 	const onsubmit = (e) => {
 		e.preventDefault();
-		console.log(surname, name, email, person, children);
+		console.log(surname, name, email, person);
 
 		if (person < 0 || person > 20) {
 			error = 'Bitte geben Sie die Anzahl der Personen an.';
-			return;
-		}
-		if (children < 0 || children > 20) {
-			error = 'Bitte geben Sie die Anzahl der Kinder an.';
 			return;
 		}
 		error = '';
@@ -46,8 +41,8 @@
 			subject += `Ich komme nicht - ${name}`;
 			body += 'Ich kann leider nicht kommen.';
 		} else {
-			subject += doSubject(person, children, name);
-			body += doText(person, children);
+			subject += doSubject(person, name);
+			body += doText(person);
 		}
 
 		body += `\nLiebe Grüße,\n${surname} ${name}`;
@@ -77,8 +72,7 @@
 	<Input id="vorname" label="Vorname" type="text" bind:value={surname} />
 	<Input id="name" label="Name" type="text" bind:value={name} />
 	<Input id="email" label="E-Mail Adresse" type="email" bind:value={email} />
-	<Input id="personen" label="Anzahl Erwachsene" type="number" bind:value={person} />
-	<Input id="kinder" label="Anzahl Kinder" type="number" bind:value={children} />
+	<Input id="personen" label="Anzahl Personen" type="number" bind:value={person} />
 	{#if error}
 		<p class="text-red-700">{error}</p>
 	{/if}
